5d7d0eba277b44097e1fb214483f677619f58bd6
san4eto <59312005+san4eto@users.noreply.github.com>
2020-02-27 23:06:05 +0100

    item creation form

--- client/src/components/CreateItem.js
+++ client/src/components/CreateItem.js
@@ -4,7 +4,12 @@ import axios from "axios";
 export default class CreateItem extends Component {
   state = {
     name: "",
-    description: ""
+    description: "",
+    category: "other",
+    expiration: Date,
+    quantity: { number: "", unit: "L" },
+    availability: "full",
+    common: "false"
   };
 
   handleChange = event => {
@@ -20,7 +25,14 @@ export default class CreateItem extends Component {
         name: this.state.name,
         description: this.state.description,
         userId: this.props.user._id,
-        fridgeId: this.props.fridgeId
+        fridgeId: this.props.fridgeId,
+        expiration: this.state.expiration,
+        image: this.state.image,
+        quantity: {
+          number: this.state.number,
+          unit: this.state.unit
+        },
+        availability: this.state.availability
       })
       .then(response => {
         console.log("response: " + response);
@@ -46,7 +58,80 @@ export default class CreateItem extends Component {
             value={this.state.name}
             onChange={this.handleChange}
           />
+          <label htmlFor="quantity">Quantity: </label>
+          <input
+            type="number"
+            id="quantity"
+            name="quantity"
+            value={this.state.number}
+            onChange={this.handleChange}
+          />
+          <label htmlFor="unit">unit </label>
+          <select
+            type="text"
+            id="unit"
+            name="unit"
+            value={this.state.unit}
+            onChange={this.handleChange}
+          >
+            <option value="L">Liter</option>
+            <option value="kg">kg</option>
+            <option value="item">item</option>
+          </select>
+          <label htmlFor="category">availability: </label>
+          <select
+            type="text"
+            id="availability"
+            name="availability"
+            value={this.state.availability}
+            onChange={this.handleChange}
+          >
+            <option value="full">full</option>
+            <option value="half-full">1/2</option>
+            <option value="almost empty">almost empty</option>
+            <option value="empty">empty</option>
+          </select>
 
+          <label htmlFor="expirydate">Best before: </label>
+          <input
+            type="date"
+            id="expirydate"
+            name="expirydate"
+            // value={this.state.expiration}
+            onChange={this.handleChange}
+          ></input>
+
+          <label htmlFor="category">Category: </label>
+
+          <select
+            type="text"
+            id="category"
+            name="category"
+            value={this.state.category}
+            onChange={this.handleChange}
+          >
+            <option value="fruit">🍏</option>
+            <option value="vegetables">🍅</option>
+            <option value="dairy">🧀</option>
+            <option value="eggs">🥚 </option>
+            <option value="meat">🥩 </option>
+            <option value="fish">🐟 </option>
+            <option value="seafood">🍤 </option>
+            <option value="dessert">🧁 </option>
+            <option value="bread">🥖</option>
+            <option value="cooked meal">🥘 </option>
+            <option value="drinks">🧃 </option>
+            <option value="alcohol">🍾 </option>
+            <option value="coffee">☕️ </option>
+            <option value="other">other </option>
+          </select>
+          <label htmlFor="common">shared food </label>
+          <input
+            type="checkbox"
+            id="common"
+            name="common"
+            value="false"
+          ></input>
           <label htmlFor="name">Description: </label>
           <input
             type="text"
