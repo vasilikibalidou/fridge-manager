b5e0fe40939d0a29f87b1bf9e47a615f3c017f56
Elsa Filippidou <elsa.filippidou@gmail.com>
2020-03-05 11:48:46 +0100

    fix bug

--- client/src/components/CreateItem.js
+++ client/src/components/CreateItem.js
@@ -10,7 +10,7 @@ export default class CreateItem extends Component {
     category: "dairy",
     expiration: null,
     number: 0,
-    unit: "",
+    unit: "L",
     availability: "full",
     common: false
   };
